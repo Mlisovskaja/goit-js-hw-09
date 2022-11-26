9ca039b78d1298556bb4963ae3bb78ce60a94257
Mari <mrs.tereshchenko@gmail.com>
2022-11-26 14:43:57 +0100

    update color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+stopBtn.setAttribute('disabled', 'disabled');
+
+function getRandomHexColor() {
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(getRandomHexColor, 1000);
+    startBtn.setAttribute('disabled', 'disabled');
+    stopBtn.removeAttribute('disabled');
+});
+
+
+stopBtn.addEventListener("click", () => {
+    stopBtn.setAttribute('disabled', 'disabled');
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+});
